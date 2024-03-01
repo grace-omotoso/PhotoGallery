9e4781981e76d28e726d2445dac28d983280d09a
Grace Omotoso <gracomot@gmail.com>
2024-02-29 19:06:26 -0500

    added project files

---
+++ app/src/main/java/com/example/photogallery/PhotoGalleryFragmentActivity.kt
@@ -0,0 +1,45 @@
+package com.example.photogallery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.GridLayout
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import api.FlickrApi
+import com.example.photogallery.databinding.FragmentPhotoGalleryBinding
+import retrofit2.Retrofit
+import retrofit2.create
+
+class PhotoGalleryFragment: Fragment() {
+    private var _binding:FragmentPhotoGalleryBinding? = null
+    private val binding
+        get() = checkNotNull(_binding){
+            "Cannot access binding because it is null. Is the view visib;e?"
+        }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentPhotoGalleryBinding.inflate(inflater, container, false)
+        binding.photoGrid.layoutManager = GridLayoutManager(context, 3)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val retrofit: Retrofit = Retrofit.Builder().baseUrl("https://www.flickr.com/").build()
+        val flickrApi: FlickrApi = retrofit.create<FlickrApi>()
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
